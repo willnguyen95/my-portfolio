d473a2fbc9c900f42d77f2761da850e34e9303d5
willnguyen95 <willnguyen120495@gmail.com>
2024-11-19 16:32:30 +0700

    add testing text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
+  <div>Testing</div>
 </template>
 
 <script lang="ts">
